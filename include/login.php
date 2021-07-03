67b82e59aa001ed5c34f88265ce1ec627b115e6b
jimmyfiv <86854033+jimmyfiv@users.noreply.github.com>
2021-07-03 17:18:56 +0700

    Update login.php

--- include/login.php
+++ include/login.php
@@ -35,6 +35,35 @@ session_start();
     <meta name="keywords" content="">
     <meta name="description" content="">
     <meta name="author" content="">
+	
+	<!-- Modernizer for Portfolio -->
+<script src="js1/modernizer.js"></script>
+<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+<!-- ALL JS FILES -->
+<script src="js1/all.js"></script>
+<!-- ALL PLUGINS -->
+<script src="js1/custom.js"></script>
+
+    <!-- Site Icons -->
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
+    <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css1/bootstrap.min.css">
+    <!-- Site CSS -->
+    <link rel="stylesheet" href="style.css">
+    <!-- Colors CSS -->
+    <link rel="stylesheet" href="css1/colors.css">
+    <!-- ALL VERSION CSS -->
+    <link rel="stylesheet" href="css1/versions.css">
+    <!-- Responsive CSS -->
+    <link rel="stylesheet" href="css1/responsive.css">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="css1/custom.css">
+
 
 </head>
 <body class="host_version"> 
